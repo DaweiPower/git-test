d0744074cbbd1c3a99eba4fc41bb74d696c18b47
kuangshen <2642479438@qq.com>
2022-08-24 16:28:49 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-24 16:25
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
