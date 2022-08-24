c19065f8d4404ffa6f6c0fc0e3ce12e4eeb0b842
kuangshen <2642479438@qq.com>
2022-08-24 16:43:33 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -9,6 +9,5 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
-        System.out.println("hello git4");
     }
 }
